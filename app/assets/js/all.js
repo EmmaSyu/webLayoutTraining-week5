91bc23728021bdae5f596afea370826cf34046db
EmmaSyu <emma831215@gmail.com>
2022-08-18 00:31:46 +0800

    version 1.0

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,54 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+
+$(document).ready(function(){
+	$('.navbar-side-link').click(function(e){
+    $('.navbar-side-link').removeClass('active');
+	  $(this).addClass('active');
+	});
+});
+
+
+// Editor
+ClassicEditor
+  .create( document.querySelector( '#editor' ) )
+  .then( editor => {
+  console.log( editor );
+} )
+  .catch( error => {
+  console.error( error );
+} );
+
+// Reply
+$(document).ready(function(){
+	$('.btn-reply').click(function(e){
+	    e.preventDefault();
+	    $(this).siblings('.btn-mark').addClass('d-none');
+      $(this).siblings('.btn-cancel').removeClass('d-none');
+      $('.editor-area').removeClass('d-none');
+      $('.reply-area').addClass('d-none');
+	});
+
+  $('.btn-cancel').click(function(e){
+	    e.preventDefault();
+	    $(this).addClass('d-none');
+	    $(this).siblings('.btn-mark').removeClass('d-none');
+      $('.editor-area').addClass('d-none');
+      $('.reply-area').removeClass('d-none');
+	});
+})
+
+
+// view-expandmore
+$(document).ready(function(){
+	$('.view-expandmore').click(function(e){
+	    $('.view-expandmore').toggleClass('d-none');
+      $('.view-expandless').toggleClass('d-none');
+	});
+  $('.view-expandless').click(function(e){
+	    $('.view-expandmore').toggleClass('d-none');
+      $('.view-expandless').toggleClass('d-none');
+	});
+})
